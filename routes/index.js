1b4cb7a324045fa6b17a19bcbcb2888a77fcf224
Alan <6691487@clases.edu.sv>
2024-07-16 14:36:46 -0600

    w

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('login/a_y_r');
 });
 
+router.get('/piola', function(req, res, next) {
+  res.render('login/index');
+});
+
 module.exports = router;
 
