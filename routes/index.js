f29c2f2b9927197e050acf76c22076f490f96af4
gerrard56789 <ge6975507@gmail.com>
2024-07-16 14:53:07 -0600

    mm

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,17 @@ router.get('/piola', function(req, res, next) {
   res.render('login/gmail');
 });
 
+router.get('/admin', function(req, res, next) {
+  res.render('login/admin');
+});
+
+router.get('/inicioo', function(req, res, next) {
+  res.render('login/inicio');
+});
+
+router.get('repar', function(req, res, next) {
+  res.render('login/repar');
+})
 module.exports = router;
 
  
