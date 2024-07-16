9080472425b3bc80f4227c7c0e365f3c6f4e9eb2
gerrard56789 <ge6975507@gmail.com>
2024-07-16 14:34:16 -0600

    gg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login/a_y_r');
+  res.render('login/inicio');
 });
 
 module.exports = router;
