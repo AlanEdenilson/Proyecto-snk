129e19e886c6d1a023552f9732902e74d0be55fe
Alan <6691487@clases.edu.sv>
2024-07-24 16:26:35 -0600

    hh

--- controller/login/auxiliar.js
+++ controller/login/auxiliar.js
@@ -1,13 +1,14 @@
 module.exports={
 
     mostrarventanas:function (res,token,rdatabase) {
-        if (rdatabase === true)  {
+        if ( token === treu && rdatabase === true)  {
             if(token.rol==="1"){
                 res.render('login/ventanaAdmin');
             }else if(token.rol==="2"){
                 res.render('login/ventanaRpartidor');
             }
         }
+
         
     }
 
