3efe1883b428fd265e599226b7e3b23ca67224a8
Alan <6691487@clases.edu.sv>
2024-07-25 09:18:10 -0600

    d

--- controller/login/auxiliar.js
+++ controller/login/auxiliar.js
@@ -1,15 +1,27 @@
+
+function sino(res,ruta) {
+    if(ruta==="1"){
+        res.render('login/ventanaAdmin');
+    }else if(ruta==="2"){
+        res.render('login/ventanaRpartidor');
+    }     
+}
+
+    
+
+
 module.exports={
 
     mostrarventanas:function (res,token,rdatabase) {
-        if (  rdatabase === true)  {
-            if(token.rol==="1"){
-                res.render('login/ventanaAdmin');
-            }else if(token.rol==="2"){
-                res.render('login/ventanaRpartidor');
-            }     
-        }
+        if ( rdatabase === true)  {
+                sino(res,token.rol);  
+    }
 
-        
+
+} , 
+    mostrarVentanas2:function (res,rol) {
+                sino(res,rol);
+    
     }
 
 }; //fin 
