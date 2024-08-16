7e457eda2cdb09d6ae8d569592eef27ad95e296e
gerrard56 <ge6975507@gmail.com>
2024-08-16 16:03:31 -0600

    h

--- public/javascript/animacion.js
+++ public/javascript/animacion.js
@@ -16,33 +16,3 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-
-
-  const modal = document.getElementById("myModall");
-  const btn = document.getElementById("openModal");
-  const span = document.getElementsByClassName("close")[0];
-  const cancelBtn = document.getElementById("cancelBtn");
-  const acceptBtn = document.getElementById("acceptBtn");
-
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
-
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  cancelBtn.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  acceptBtn.onclick = function() {
-      alert("Acción aceptada");
-      modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
