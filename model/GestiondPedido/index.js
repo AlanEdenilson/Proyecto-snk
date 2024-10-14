552829719cf4b196cf372380010ccb997e0ea278
Alan <6691487@clases.edu.sv>
2024-10-14 12:37:24 -0600

    eee

--- model/GestiondPedido/index.js
+++ model/GestiondPedido/index.js
@@ -153,36 +153,45 @@ module.exports = {
         conexion.query(sql, funcion);
     },
     loadContent: async function (conexion,marca, funcion) {
-        console.log('buscando pedidos exitosos...'+marca)
+        console.log('buscando pedidos en procesos...'+marca)
         const sql = `
-        SELECT
-            m.id AS marca_id,
-            pa.estado_vendedor,
-            pa.estado,
-            DATE_FORMAT(pa.fecha_pedido, '%Y-%m-%d %H:%i:%s') AS fecha_hora_pedido,
-            pa.id AS pedido_id,
-            pa.repartidor_id AS repartidor,
-            DATE_FORMAT(pa.fecha_estimada_entrega, '%Y-%m-%d %H:%i:%s') AS fecha_entrega,
-            SUM(dp.subtotal) AS total_pedido,
-            GROUP_CONCAT(DISTINCT CONCAT(
-                'producto_id:', dp.producto_id,
-                ',precio:', dp.precio_unitario,
-                ',cantidad:', dp.cantidad
-            ) SEPARATOR '||') AS detalles_productos,
-            SUM(dp.cantidad) AS total_cantidad
-        FROM
+        SELECT 
+    m.id AS marca_id,
+    u.usuario AS nombre_repartidor,
+    pa.estado_vendedor,
+    
+
+
+    GROUP_CONCAT(DISTINCT pa.estado) AS estados,
+    DATE_FORMAT(pa.fecha_pedido, '%Y-%m-%d %H:%i:%s') AS fecha_hora_pedido,
+    GROUP_CONCAT(DISTINCT pa.id) AS pedidos_ids,
+    pa.repartidor_id AS repartidor,
+    DATE_FORMAT(pa.fecha_estimada_entrega, '%Y-%m-%d %H:%i:%s') AS fecha_entrega,
+
+    SUM(dp.subtotal) AS total_pedido,
+    GROUP_CONCAT(DISTINCT CONCAT(
+        'producto_id:', dp.producto_id, 
+        ',precio:', dp.precio_unitario, 
+        ',cantidad:', dp.cantidad
+    ) SEPARATOR '||') AS detalles_productos,
+    SUM(dp.cantidad) AS total_cantidad
+    
+
+    
+        FROM 
             marcas m
-            JOIN productos p ON m.id = p.marca_id
-            JOIN detalles_pedido dp ON p.id = dp.producto_id
-            JOIN pedidos_activos pa ON dp.pedido_id = pa.id
-        WHERE
-            m.id = ${marca}  
-        GROUP BY
-            pa.id 
-        ORDER BY
-            pa.fecha_pedido DESC;
-`;
-        conexion.query(sql, funcion)
+        JOIN productos p ON m.id = p.marca_id
+        JOIN detalles_pedido dp ON p.id = dp.producto_id
+        JOIN pedidos_activos pa ON dp.pedido_id = pa.id
+        JOIN usuarios u ON pa.repartidor_id = u.id
+
+        WHERE 
+            m.id = ${marca} 
+        GROUP BY 
+
+        m.nombre, fecha_hora_pedido DESC;
+        `;
+    conexion.query(sql, funcion)
 
     },
 
